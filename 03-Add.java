792f343d86d22e04f7edaa4826900304d3d0b5a0
Yuvii2102 <yuvrajmysuru@gmail.com>
2026-01-31 21:11:48 +0530

    Create 03-Add.java

---
+++ 03-Add.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;                                                           // whenever we use sc.nextInt() remember t use Scanner sc = new Scanner(System.in);
+public class Main
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        
+        System.out.println("Enter first num:");
+        int a = sc.nextInt();
+        
+        System.out.println("Enter second num:");
+        int b = sc.nextInt();
+        
+        int sum=a+b;
+        System.out.println("Sum of two numbers is:" +sum);
+    }
+}
