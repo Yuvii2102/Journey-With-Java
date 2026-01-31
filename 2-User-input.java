75fbd3363d57e1318052be25a47d31ccf0f677e3
Yuvii2102 <yuvrajmysuru@gmail.com>
2026-01-31 20:54:59 +0530

    Create 2-User-input.java

---
+++ 2-User-input.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Userinput
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter your name:");
+        System.out.println("name:");
+    }
+}
