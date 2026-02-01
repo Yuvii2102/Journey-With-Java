88ea18273d5d7d99da61dc96054541929225ffb2
Yuvii2102 <yuvrajmysuru@gmail.com>
2026-02-01 12:53:37 +0530

    Create 8-Swap.java

---
+++ 8-Swap.java
@@ -0,0 +1,47 @@
+//With Temp
+
+import java.util.Scanner;
+public class Main
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        
+        System.out.println("Enter two numbers:");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        
+        int temp = a;
+        a=b;
+        b=temp;
+        
+        System.out.println("After swapping:");
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
+
+
+//Without Temp
+
+import java.util.Scanner;
+public class Main
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        
+        System.out.println("Enter two numbers:");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+       
+        a = a+b;
+        b = a-b;
+        a = a-b;
+        
+        System.out.println("After swapping:");
+        System.out.println(a);
+        System.out.println(b);
+       
+    }
+}
