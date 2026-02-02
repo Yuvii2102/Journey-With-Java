5e32f505043dcdfe692555a03e5c110ba9f65060
Yuvii2102 <yuvrajmysuru@gmail.com>
2026-02-02 19:47:08 +0530

    Create 04-Largest-Of-Three.java

---
+++ Level-02/04-Largest-Of-Three.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;                                                                    
+public class Main
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter three numbers:");
+    
+        
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        
+        if(a>b)
+         System.out.println("a is larger:"+a);
+        else if( b>a)
+         System.out.println("b is larger:"+b);
+        else
+         System.out.println("c is larger:"+c);
+    }
+}
+
+
+// Another method used :- 
+
+import java.util.Scanner;
+public class Main
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter three numbers:");
+    
+        
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        
+        if (a>=b && a>=c)
+         System.out.println("a is larger:"+a);
+        else if (b>=a && b>=c)
+         System.out.println("b is larger:"+b);
+        else
+         System.out.println("c is larger:"+c);
+    }
+}
+
+
+                     
