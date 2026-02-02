6c384d81e61a2f2ef9362d75b5dc3a6b33b519a1
Yuvii2102 <yuvrajmysuru@gmail.com>
2026-02-02 19:52:25 +0530

    Create 05-Smallest-Of-Two.java

---
+++ Level-02/05-Smallest-Of-Two.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Main
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter two numbers:");
+    
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        
+        if (a < b)
+         System.out.println("a is smaller:" +a);
+        else
+         System.out.println("b is smaller:" +b);
+      
+    }
+}
