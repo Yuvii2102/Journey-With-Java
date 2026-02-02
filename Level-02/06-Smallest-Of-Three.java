54450999f5db156c86969277fcdb72c36bbc9ae6
Yuvii2102 <yuvrajmysuru@gmail.com>
2026-02-02 19:55:35 +0530

    Create 06-Smallest-Of-Three.java

---
+++ Level-02/06-Smallest-Of-Three.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;                                                   // if-else                                                 
+public class Main
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter three numbers:");
+    
+        
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        
+        if(a < b)
+         System.out.println("a is smaller:"+a);
+        else if( b < a)
+         System.out.println("b is smaller:"+b);
+        else
+         System.out.println("c is smaller:"+c);
+    }
+}
+
+
+
+// Another method used :- 
+
+import java.util.Scanner;                                                   // ternary operator
+public class Main
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter three numbers:");
+    
+        
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        
+        if (a<=b && a<=c)
+         System.out.println("a is smaller:"+a);
+        else if (b<=a && b<=c)
+         System.out.println("b is smaller:"+b);
+        else
+         System.out.println("c is smaller:"+c);
+    }
+}
+
+
+                     
