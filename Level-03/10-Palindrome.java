9ed81c3503f2c4591a3c8f666e68577372c6efa4
Yuvraj.R <yuvrajmysuru@gmail.com>
2026-02-04 19:23:20 +0530

    Create 10-Palindrome.java

---
+++ Level-03/10-Palindrome.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class Main
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        int original = n;
+        int rev = 0;
+    
+        
+        while(n>0)
+        {
+          rev = rev * 10 + (n%10);
+          n= n / 10;
+        }
+      
+        
+        if (original == rev)
+        {
+          System.out.println("Palindrome");
+        }
+        else
+        {
+          System.out.println("Not a Palindrome");
+        }
+    
+            
+     }
+}
