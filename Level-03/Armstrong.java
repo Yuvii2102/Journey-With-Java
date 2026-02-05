01be24f1bf2ecfbc9ed6e134815de1a7e6736395
Yuvraj.R <yuvrajmysuru@gmail.com>
2026-02-05 17:27:19 +0530

    Create Armstrong.java

---
+++ Level-03/Armstrong.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String args[])
+    {
+        int n = 155;
+        int temp = n;
+        int r, sum=0;
+        
+        while(n>0)
+        {
+            r = n % 10;
+            n = n / 10;
+            sum = sum + r*r*r;
+        }
+        
+        if(temp == sum)
+         System.out.println("Its Armstrong");
+        else
+         System.out.println("Not an Armstrong");
+         
+    }
+}
