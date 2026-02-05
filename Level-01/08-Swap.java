56047783ef38c23b820b89e11a162ea06d14a025
Yuvraj.R <yuvrajmysuru@gmail.com>
2026-02-05 18:11:09 +0530

    Update 08-Swap.java

--- Level-01/08-Swap.java
+++ Level-01/08-Swap.java
@@ -45,3 +45,53 @@ public class Main
        
     }
 }
+
+
+// Swap using third variable by user input
+
+import java.util.Scanner;
+public class Main
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        
+        System.out.println("Enter two numbers:");
+        int a = 10;
+        int b = 20;
+        
+        int temp = a;
+        a=b;
+        b=temp;
+        
+         System.out.println(a);
+         System.out.println(b);
+       
+    }
+}
+
+
+// Swap without using third variable
+
+import java.util.Scanner;
+public class Main
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        
+        System.out.println("Enter two numbers:");
+        int a = 10;
+        int b = 20;
+       
+        a = a+b;
+        b = a-b;
+        a = a-b;
+        
+        System.out.println(a);
+        System.out.println(b);
+       
+    }
+}
+
+
