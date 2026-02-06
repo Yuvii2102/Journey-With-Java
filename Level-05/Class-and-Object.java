d2caefa2f3f00a7aa568a59ac942872f7db4699c
Yuvraj.R <yuvrajmysuru@gmail.com>
2026-02-06 19:05:50 +0530

    Create Class-and-Object.java

---
+++ Level-05/Class-and-Object.java
@@ -0,0 +1,25 @@
+class Student
+{
+  String name;
+  int age;
+  
+  void display()
+  {
+    System.out.println("Name:"+name);
+    System.out.println("Age:"+age);
+  }
+}
+
+
+public class Main
+{
+  public static void main(String args[])
+  {
+    Student s1 = new Student();                // Object creation 
+    
+    s1.name = "Yuva";
+    s1.age = 19;
+    
+    s1.display();                              // Method call 
+  } 
+}
