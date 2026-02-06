ca0fd11943c207d3e333fa7462a16cb09f1fa527
Yuvraj.R <yuvrajmysuru@gmail.com>
2026-02-06 17:02:58 +0530

    Create 02-Largest-element-array.java

---
+++ Level-04/02-Largest-element-array.java
@@ -0,0 +1,20 @@
+public class LargestElement
+{
+  public static void main(String args[])
+  {
+    int arr[] = {10,45,22,99,5};
+    
+    int largest = arr[0];
+    
+    for(int i=1; i<arr.length; i++)
+    {
+      if(arr[i]>largest)
+      {
+        largest=arr[i];
+      }
+    }
+    System.out.println("largest element:"+largest);
+  }
+}
+    
+    
