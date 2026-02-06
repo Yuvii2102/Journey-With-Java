6f0fb557d435b90fa514fc5d242e4150b6846392
Yuvraj.R <yuvrajmysuru@gmail.com>
2026-02-06 17:31:18 +0530

    Create 06-Sort-array.java

---
+++ Level-04/06-Sort-array.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+public class ArrayAverage
+{
+  public static void main(String args[])
+  {
+    int arr[] = {10,80,20,40};
+    
+    int sum = 0;
+    
+    Arrays.sort(arr);
+    
+    System.out.println(Arrays.toString(arr));
+    
+  }
+}
+    
