ad3c1ea0dd2a3642a7736154ee83175a581849b6
Yuvraj.R <yuvrajmysuru@gmail.com>
2026-02-06 16:57:59 +0530

    Add Arraycount class to count array elements

---
+++ Level-04/01-Num-of-Array.java
@@ -0,0 +1,11 @@
+public class Arraycount
+{
+  public static void main(String args[])
+  {
+    int arr[] = {10,20,30,40,50};
+    
+    int count = arr.length;
+    
+    System.out.println("Number of elements in array:"+count);
+  }
+}
