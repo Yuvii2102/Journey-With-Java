eb98cfe384d0cc76e3000b515f8aab80caa58bd6
Yuvraj.R <yuvrajmysuru@gmail.com>
2026-02-06 18:50:26 +0530

    Create 02-Replace-a-Char-to-String.java

---
+++ Level-05/02-Replace-a-Char-to-String.java
@@ -0,0 +1,12 @@
+public class StringLength
+{
+  public static void main(String args[])
+  {
+    String str = "Cherry";
+    
+    String result = str.replace('r','o');
+    
+    System.out.println("New string:" +result);
+    
+  }
+}
