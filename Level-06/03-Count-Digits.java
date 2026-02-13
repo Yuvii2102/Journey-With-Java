af86cca47ee02d6cd35483d528305e39cd523f48
Yuvraj.R <yuvrajmysuru@gmail.com>
2026-02-13 17:57:40 +0530

    Create 03-Count-Digits.java

---
+++ Level-06/03-Count-Digits.java
@@ -0,0 +1,12 @@
+public class CountDigits
+{
+    public static void main(String args[])
+    {
+      int num = 12345;
+      
+      int digits = String.valueOf(num).length();
+      
+      System.out.println("Number of digits:"+digits);
+      
+    }
+}
