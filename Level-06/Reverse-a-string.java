c07885673757df2b63a7b1f99b0a126eed5970c0
Yuvraj.R <yuvrajmysuru@gmail.com>
2026-02-13 17:36:06 +0530

    Create Reverse-a-string.java

---
+++ Level-06/Reverse-a-string.java
@@ -0,0 +1,11 @@
+public class ReverseString
+{
+    public static void main(String[] args) 
+  {
+        
+        String str = "Automation" ;
+        StringBuilder reversed = new StringBuilder(str).reverse();
+        System.out.println(reversed);
+    
+    }
+}
