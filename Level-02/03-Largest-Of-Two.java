3bdcceff297f0af4c0582ec82491ec318bdf9e38
Yuvraj.R <yuvrajmysuru@gmail.com>
2026-02-26 18:00:26 +0530

    Update 03-Largest-Of-Two.java

--- Level-02/03-Largest-Of-Two.java
+++ Level-02/03-Largest-Of-Two.java
@@ -16,3 +16,27 @@ public class Main
       
     }
 }
+
+
+
+
+import java.util.Scanner;
+public class Main
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+       
+        System.out.println("Enter a:");
+        int a = sc.nextInt();
+        System.out.println("Enter b:");
+        int b = sc.nextInt();
+        
+        
+        if(a>b)
+        System.out.println("A is greater:"+a);
+        else if(a<b)
+        System.out.println("B is greater:"+b);
+        
+    }
+}
