507d750fd549532167d0a00c1f6311535bb3fc99
Yuvraj.R <yuvrajmysuru@gmail.com>
2026-03-01 11:55:11 +0530

    Update 12-Alpha, Digit-or-Character.java

--- Level-02/12-Alpha, Digit-or-Character.java
+++ Level-02/12-Alpha, Digit-or-Character.java
@@ -10,11 +10,13 @@ public class Main
         System.out.println("Enter a character:");
         char ch = sc.next().charAt(0);
 
-        if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z')) {
+        if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z')) 
+        {
             System.out.println("is an alphabet");
         }
         
-        else if (ch >= '0' && ch <= '9') {
+        else if (ch >= '0' && ch <= '9') 
+        {
             System.out.println("is a digit");
             
         }
@@ -23,6 +25,26 @@ public class Main
         
         {
             System.out.println("is a special character");
+        }
+    }
+}
+
+
+// If you want Only Alphabet 
+
+import java.util.Scanner;
+public class Main
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Enter the number:");
+        char ch = sc.next().charAt(0);
+        
+        if(ch>='a' && ch<='z' || (ch>='A' && ch<='Z'))
+        {
+            System.out.println("Is an alphabet ");
         }
     }
 }
