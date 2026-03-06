54561a4967928ecc84c269ded068c6c27fb9bfe6
Yuvraj.R <yuvrajmysuru@gmail.com>
2026-03-06 20:26:18 +0530

    Create 09-Binary-Search.java

---
+++ Level-04/09-Binary-Search.java
@@ -0,0 +1,36 @@
+public class BinarySearch
+{
+  public static int binarySearch(int arr[], int key)
+  {
+    int low = 0;
+    int high = arr.length - 1;
+
+    while(low <= high)
+    {
+      int mid = (low + high) / 2;
+
+      if(arr[mid] == key)
+      {
+        return mid;
+      }
+      else if(arr[mid] < key)
+      {
+        low = mid + 1;
+      }
+      else
+      {
+        high = mid - 1;
+      }
+    }
+
+    return -1;
+  }
+
+  public static void main(String args[])
+  {
+    int arr[] = {10,20,30,40,50};
+    int key = 30;
+
+    System.out.println(binarySearch(arr, key));
+  }
+}
