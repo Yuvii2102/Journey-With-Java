be965de88abe58fdde90999644719849a6fce8f1
Yuvraj.R <yuvrajmysuru@gmail.com>
2026-03-06 12:51:50 +0530

    Update 12-Prime.java

--- Level-03/12-Prime.java
+++ Level-03/12-Prime.java
@@ -4,6 +4,45 @@ public class Main
 {
     public static void main(String args[])
     {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int count = 0;
+
+        for(int i = 1; i <= n; i++)
+        {
+            if(n % i == 0)
+            {
+                count++;
+            }
+        }
+
+        if(count == 2)
+        {
+            System.out.println("Prime");
+        }
+        else
+        {
+            System.out.println("Not Prime");
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String args[])
+    {
         int n=10;
         boolean prime = true;
         
