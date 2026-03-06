d57fa9d6d2effcb5de0f8a41b0fabd8eea2a91cc
Yuvraj.R <yuvrajmysuru@gmail.com>
2026-03-06 13:51:05 +0530

    Update 17-Sum-of-Digits.java

--- Level-03/17-Sum-of-Digits.java
+++ Level-03/17-Sum-of-Digits.java
@@ -4,6 +4,29 @@ public class Main
 {
     public static void main(String args[])
     {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int sum = 0;
+
+        while(n > 0)
+        {
+            int r = n % 10;
+            sum = sum + r;
+            n = n / 10;
+        }
+
+        System.out.println(sum);
+    }
+}
+
+
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String args[])
+    {
        Scanner sc = new Scanner(System.in);
        int n =  sc.nextInt();
        
