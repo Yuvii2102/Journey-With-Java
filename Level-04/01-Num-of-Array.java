23329b952065d5af3d16e06492ec2816f6a9053d
Yuvraj.R <yuvrajmysuru@gmail.com>
2026-03-06 18:26:06 +0530

    Update 01-Num-of-Array.java

--- Level-04/01-Num-of-Array.java
+++ Level-04/01-Num-of-Array.java
@@ -1,3 +1,5 @@
+// Number Of Elements in an Array
+
 public class Arraycount
 {
   public static void main(String args[])
@@ -9,3 +11,26 @@ public class Arraycount
     System.out.println("Number of elements in array:"+count);
   }
 }
+
+// Another Method with example
+
+public class ArrayLengthExample 
+{
+    public static void main(String[] args) 
+  {
+        // Declare and initialize an array of integers
+        int numbers[] = {10, 20, 30, 40, 50};
+
+        // Use the length property to get the number of elements
+        int size = numbers.length;
+
+        // Print the size of the array
+        System.out.println("The number of elements in the array is: " + size);
+    
+        String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+        System.out.println("The number of elements in the cars array is: " + cars.length);
+    
+    }
+  }
+
+        
