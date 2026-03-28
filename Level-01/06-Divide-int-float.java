8c93334f7ba0126ae4f234798f4f705d27b0f160
Yuvraj.R <yuvrajmysuru@gmail.com>
2026-03-28 17:36:31 +0530

    Update 06-Divide-int-float.java

--- Level-01/06-Divide-int-float.java
+++ Level-01/06-Divide-int-float.java
@@ -1,36 +1,20 @@
-import java.util.Scanner;                                           // using integer
+import java.util.Scanner;
 public class Main
 {
     public static void main(String args[])
     {
         Scanner sc = new Scanner(System.in);
         
-        System.out.println("Enter two numbers:");
-        
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        
-        int divint = a/b;
-        
-        System.out.println("Integer divison="+divint);
-    }
-}
-
-
-import java.util.Scanner;                                        // using float
-public class Main
-{
-    public static void main(String args[])
-    {
-        Scanner sc = new Scanner(System.in);
-        
-        System.out.println("Enter two numbers:");
-        
+        System.out.println("Enter first num:");
         float a = sc.nextFloat();
+        
+        System.out.println("Enter second num:");
         float b = sc.nextFloat();
         
-        float divfloat = a/b;
-        
-        System.out.println("Float divison="+divfloat);
+        float div = a/b;
+        System.out.println("Result:"+div);
     }
 }
+
+
+// use either int or float
