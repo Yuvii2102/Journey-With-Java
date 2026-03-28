3a0b6727aebdf1aab1c32deff0df8cabee6d58ee
Yuvraj.R <yuvrajmysuru@gmail.com>
2026-03-28 16:30:23 +0530

    Update 01-Reverse-a-string.java

--- Level-06/01-Reverse-a-string.java
+++ Level-06/01-Reverse-a-string.java
@@ -1,3 +1,21 @@
+public class ReverseString 
+{
+    public static void main(String[] args) 
+    {
+        String str = "yuvi";
+        String rev = "";
+
+        for (int i = str.length() - 1; i >= 0; i--) 
+        {
+            rev = rev + str.charAt(i);
+        }
+
+        System.out.println("Reversed: " + rev);
+    }
+}
+
+// OR
+
 public class ReverseString
 {
     public static void main(String[] args) 
