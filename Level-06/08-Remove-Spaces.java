e39bc0405080fde856417df5b6be526f9476417f
Yuvraj.R <yuvrajmysuru@gmail.com>
2026-03-28 16:31:03 +0530

    Create 08-Remove-Spaces.java

---
+++ Level-06/08-Remove-Spaces.java
@@ -0,0 +1,11 @@
+public class RemoveSpaces 
+{
+    public static void main(String[] args) 
+  {
+        String str = "yuvi is cool";
+
+        String result = str.replaceAll(" ", "");
+
+        System.out.println(result);
+    }
+}
