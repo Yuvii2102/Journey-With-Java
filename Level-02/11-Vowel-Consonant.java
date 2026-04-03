a20fe6120080c113ba52f63c03a52bd3af9fa109
Yuvraj.R <yuvrajmysuru@gmail.com>
2026-04-03 12:56:29 +0530

    Update 11-Vowel-Consonant.java

--- Level-02/11-Vowel-Consonant.java
+++ Level-02/11-Vowel-Consonant.java
@@ -9,10 +9,14 @@ public class Main {
         char ch = sc.next().charAt(0);
 
         if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u'
-         || ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U') {
-
+         || ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U')
+            
+        
+        {
             System.out.println("is a vowel");
-        } else {
+        } 
+        else 
+        {
             System.out.println("is not a vowel");
         }
     }
