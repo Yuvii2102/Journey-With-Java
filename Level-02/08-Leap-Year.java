68eb0d2ac3f405d12ec2110f8590f9b61697f586
Yuvraj.R <yuvrajmysuru@gmail.com>
2026-04-03 11:30:06 +0530

    Update 08-Leap-Year.java

--- Level-02/08-Leap-Year.java
+++ Level-02/08-Leap-Year.java
@@ -1,16 +1,25 @@
-import java.util.Scanner;                                                 
+import java.util.Scanner;
+
 public class Main
 {
     public static void main(String args[])
     {
-        Scanner sc=new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         System.out.println("Enter the year:");
         
         int year = sc.nextInt();
-        if ((year % 400 == 0) || (year %4 == 0 && year % 100! = 0))
-         System.out.println("Is a leap year:"+year);
-        else
-         System.out.println("Not a leap year:"+year);
 
+        if(year % 400 == 0)
+        {
+            System.out.println("Leap year");
+        }
+        else if(year % 100 == 0)
+        {
+            System.out.println("Not a leap year");
+        }
+        else if(year % 4 == 0)
+        {
+            System.out.println("Leap year");
+        }
     }
 }
